d50b9fde074d9c61bb88b85435a1a60f5237b9c4
ennmichael <enntheprogrammer@gmail.com>
2021-08-15 18:19:21 +0200

    Added a proper animation abstraction

--- zelda.rb
+++ zelda.rb
@@ -14,71 +14,121 @@ module Zelda
 
     private_constant :SPRITE_SCALE
 
-    # Link's sprites.
-    class LinkSprites
-      WALK_ANIMATION_SPEED = 0.005
-      WALK_TILES = 4
+    # A Sprite is a single frame within an animation.
+    class Sprite
+      attr_reader :duration_ms
 
-      private_constant :WALK_ANIMATION_SPEED, :WALK_TILES
+      def initialize(image, scale_x, scale_y, duration_ms)
+        Contracts.not_nil image
+        Contracts.not_nil scale_x
+        Contracts.not_nil scale_y
+        Contracts.not_nil duration_ms
+        Contracts.is image, Gosu::Image
+        Contracts.is scale_x, Numeric
+        Contracts.is scale_y, Numeric
+        Contracts.is duration_ms, Numeric
 
-      def initialize
-        @walk = Gosu::Image.load_tiles 'media/link_walk.bmp', -WALK_TILES, -1
-        @last_sprite = @walk.first
-        @last_scale = SPRITE_SCALE
+        @image = image
+        @scale_x = scale_x
+        @scale_y = scale_y
+        @duration_ms = duration_ms
       end
 
-      def draw_facing_up(x, y)
-        Contracts.position x, y
-
-        @last_sprite = sprite_walk_up
-        @last_scale = ((Gosu.milliseconds * WALK_ANIMATION_SPEED).round % 2).zero? ? SPRITE_SCALE : -SPRITE_SCALE
-        draw_last x, y
+      def flipped
+        Sprite.new @image, -@scale_x, @scale_y, @duration_ms
       end
 
-      def draw_facing_down(x, y)
-        Contracts.position x, y
-
-        @last_sprite = sprite_walk_down
-        @last_scale = ((Gosu.milliseconds * WALK_ANIMATION_SPEED).round % 2).zero? ? SPRITE_SCALE : -SPRITE_SCALE
-        draw_last x, y
+      def infinite
+        Sprite.new @image, @scale_x, @scale_y, Float::INFINITY
       end
 
-      def draw_facing_left(x, y)
-        Contracts.position x, y
+      def draw(x, y)
+        @image.draw_rot x, y, 0, 0, 0.5, 0.5, @scale_x * SPRITE_SCALE, @scale_y * SPRITE_SCALE
+      end
+    end
 
-        @last_sprite = sprites_walk_left[Gosu.milliseconds * WALK_ANIMATION_SPEED % 2]
-        @last_scale = SPRITE_SCALE
-        draw_last x, y
+    # An animation is a set of sprites playing in a loop.
+    # Animations have state and can change sprites.
+    class Animation
+      def initialize(animation_map)
+        animation_map.each_pair do |key, value|
+          Contracts.not_nil key
+          Contracts.not_nil value
+          Contracts.is key, Symbol
+          Contracts.is value, Array
+          value.each do |x|
+            Contracts.not_nil x
+            Contracts.is x, Sprite
+          end
+        end
+
+        @animation_map = animation_map
+        @active_animation = animation_map.first.first
+        @active_frame = 0
+        @last_draw = Gosu.milliseconds
+        reset_remaining_duration
       end
 
-      def draw_facing_right(x, y)
-        Contracts.position x, y
+      def set(animation)
+        Contracts.is animation, Symbol
+        Contracts.not_nil animation
+        Contracts.includes animation, @animation_map.keys
 
-        @last_sprite = sprites_walk_left[Gosu.milliseconds * WALK_ANIMATION_SPEED % 2]
-        @last_scale = -SPRITE_SCALE
-        draw_last x, y
+        return if @active_animation == animation
+
+        @active_animation = animation
+        @active_frame = 0
+        reset_remaining_duration
       end
 
-      def draw_last(x, y)
-        @last_sprite.draw_rot x, y, 0, 0, 0.5, 0.5, @last_scale, SPRITE_SCALE
+      def draw(x, y)
+        now = Gosu.milliseconds
+        delta = now - @last_draw
+        @last_draw = now
+        @remaining_duration -= delta
+
+        advance_frame if @remaining_duration <= 0
+
+        active_sprite.draw x, y
       end
 
       private
 
-      def sprite_walk_down
-        @walk[0]
+      def active_sprite
+        @animation_map[@active_animation][@active_frame]
       end
 
-      def sprite_walk_up
-        @walk[1]
+      def reset_remaining_duration
+        @remaining_duration = active_sprite.duration_ms
       end
 
-      def sprites_walk_left
-        @walk[2, 3]
+      def advance_frame
+        @active_frame += 1
+        @active_frame %= @animation_map[@active_animation].length
+        reset_remaining_duration
       end
     end
 
-    class BlockSprites
+    class << self
+      def link_animation
+        animation_duration = 100
+        down, up, left0, left1 = Gosu::Image.load_tiles('media/link_walk.bmp', -4, -1).map do |tile|
+          Sprite.new tile, 1, 1, animation_duration
+        end
+
+        Animation.new(
+          {
+            walk_left: [left1, left0],
+            walk_right: [left1.flipped, left0.flipped],
+            walk_up: [up, up.flipped],
+            walk_down: [down, down.flipped],
+            idle_left: [left0.infinite],
+            idle_right: [left0.flipped.infinite],
+            idle_up: [up.flipped.infinite],
+            idle_down: [down.flipped.infinite]
+          }
+        )
+      end
     end
   end
 
@@ -98,8 +148,10 @@ module Zelda
 
       @grid = grid
       @entity = entity
-      @sprites = Resources::LinkSprites.new
+      @animation = Resources.link_animation
       @x, @y = target_position
+      self.direction = :right
+      @walking = false
     end
 
     def update(requested_direction)
@@ -116,21 +168,20 @@ module Zelda
     def draw
       case direction
       when :up
-        @sprites.draw_facing_up @x, @y
+        @animation.set(@walking ? :walk_up : :idle_up)
       when :down
-        @sprites.draw_facing_down @x, @y
+        @animation.set(@walking ? :walk_down : :idle_down)
       when :left
-        @sprites.draw_facing_left @x, @y
+        @animation.set(@walking ? :walk_left : :idle_left)
       when :right
-        @sprites.draw_facing_right @x, @y
-      else
-        @sprites.draw_last @x, @y
+        @animation.set(@walking ? :walk_right : :idle_right)
       end
+
+      @animation.draw @x, @y
     end
 
     private
 
-    # TODO: I don't need this. Direction can be determined entirely by looking at current vs target position
     attr_writer :direction
 
     def target_position
@@ -140,14 +191,16 @@ module Zelda
     end
 
     def update_still(requested_direction)
+      @walking = false
       return if requested_direction.nil? || !@grid.move(@entity, requested_direction)
 
+      @walking = true
       self.direction = requested_direction
     end
 
     def update_moving(target_x, target_y)
+      @walking = false if @x == target_x && @y == target_y
       advance_toward target_x, target_y
-      self.direction = nil if @x == target_x && @y == target_y
     end
 
     def advance_toward(target_x, target_y)
